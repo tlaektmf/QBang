42d1853955ab3e90a08570b5c5dfb13474d7746d
tlaektmf <tlaektmf0826@gmail.com>
2019-11-12 01:09:46 +0900

    Update VM_RegisterProblemActivity.java

--- app/src/main/java/com/example/visualmath/VM_RegisterProblemActivity.java
+++ app/src/main/java/com/example/visualmath/VM_RegisterProblemActivity.java
@@ -384,9 +384,10 @@ public class VM_RegisterProblemActivity extends AppCompatActivity {
 
     public void registerProblem(View view) {
         //** 데이터베이스에 저장
-        vmDataBasic.setTitle(editTextTitle.getText().toString());
-        VM_DBHandler vmDbHandler=new VM_DBHandler("POSTS");
-        vmDbHandler.newPost(receiveData,vmDataBasic);
+
+//        vmDataBasic.setTitle(editTextTitle.getText().toString());
+//        VM_DBHandler vmDbHandler=new VM_DBHandler("POSTS");
+//        vmDbHandler.newPost(receiveData,vmDataBasic);
 
         finish();
     }
