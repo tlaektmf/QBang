741f19008a551a95eb28c0c1aca340df2a608c39
tlaektmf <tlaektmf0826@gmail.com>
2019-11-12 01:04:05 +0900

    Update VM_Data_ADD.java

--- app/src/main/java/com/example/visualmath/VM_Data_ADD.java
+++ app/src/main/java/com/example/visualmath/VM_Data_ADD.java
@@ -27,10 +27,9 @@ public class VM_Data_ADD implements Parcelable {
         int lenght =3;
         filePathList=new Uri[lenght];
 
-        //** 사진을 0번째부터 정렬하고 싶으면 이 코드 오픈
-//        for(int i=0;i<lenght;i++){
-//            filePathList[i]=(Uri)in.readParcelable(null);//Parcelable로 읽어서 Uri로 캐스트하여 데이터를 복원함
-//        }
+        for(int i=0;i<lenght;i++){
+            filePathList[i]=(Uri)in.readParcelable(null);//Parcelable로 읽어서 Uri로 캐스트하여 데이터를 복원함
+        }
 
     }
 
@@ -59,9 +58,14 @@ public class VM_Data_ADD implements Parcelable {
 
         // Uri는 Parcelable을 이용해서 기록
         for(int i=0; i<3; i++){
-            if(filePathList[i]!=null){
-                dest.writeParcelable(filePathList[i],flags);
-            }
+
+            //** 사진을 0번째부터 정렬하고 싶으면 이 코드 open
+//            if(filePathList[i]!=null){
+//                dest.writeParcelable(filePathList[i],flags);
+//            }
+
+            dest.writeParcelable(filePathList[i],flags);
+
         }
 
     }
