db04c5bfee8d1c5c1b23590eb8d0cd1ca736d08e
tlaektmf <tlaektmf0826@gmail.com>
2019-11-25 06:21:26 +0900

    문제풀기뷰 생성 및 작업완료

--- app/src/main/java/com/example/visualmath/TeacherProblemFragment.java
+++ app/src/main/java/com/example/visualmath/TeacherProblemFragment.java
@@ -1,7 +1,6 @@
 package com.example.visualmath;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -15,19 +14,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.BitSet;
 import java.util.Vector;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProblemFragment extends Fragment {
-
-    private static final String TAG = "ProblemFragment";
+public class TeacherProblemFragment extends Fragment {
+    private static final String TAG = "TeacherProblemFragment";
     ViewGroup rootView;
     private Vector<VM_Data_CHAT> chats;
     private VM_ChatAdapter adapter;
@@ -37,9 +33,9 @@ public class ProblemFragment extends Fragment {
     private Button sendMsgBtn;
     private Button showActionDialog;
 
-    public ProblemFragment() {
-        // Required empty public constructor
 
+    public TeacherProblemFragment() {
+        // Required empty public constructor
     }
 
 
@@ -47,15 +43,14 @@ public class ProblemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        rootView=(ViewGroup)inflater.inflate(R.layout.fragment_problem, container, false);
-        View view=inflater.inflate(R.layout.fragment_problem, container, false);
+        rootView=(ViewGroup)inflater.inflate(R.layout.fragment_teacher_problem, container, false);
 
         RecyclerView recyclerView=(RecyclerView)rootView.findViewById(R.id.chatRoomListView);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(linearLayoutManager);
         chats = new Vector<>();
 
-       ceateData();
+        ceateData();
 
         adapter = new VM_ChatAdapter( chats,getActivity());
         recyclerView.setAdapter(adapter);
@@ -64,9 +59,9 @@ public class ProblemFragment extends Fragment {
                 linearLayoutManager.getOrientation());
         recyclerView.addItemDecoration(dividerItemDecoration);
 
-        msgEditText=rootView.findViewById(R.id.msgEditText);
-        sendMsgBtn=rootView.findViewById(R.id.sendMsgBtn);
-        showActionDialog=rootView.findViewById(R.id.showActionDialog);
+        msgEditText=rootView.findViewById(R.id.teacher_msgEditText);
+        sendMsgBtn=rootView.findViewById(R.id.teacher_sendMsgBtn);
+        showActionDialog=rootView.findViewById(R.id.teacher_showActionDialog);
 
         sendMsgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +69,7 @@ public class ProblemFragment extends Fragment {
                 Log.i(TAG,"okay");
                 count++;
 
-                VM_Data_CHAT data=new VM_Data_CHAT("근육몬", msgEditText.getText().toString(),0, 0);
+                VM_Data_CHAT data=new VM_Data_CHAT("괴력몬", msgEditText.getText().toString(),0, 0);
                 chats.add(data);
                 adapter.notifyDataSetChanged();
             }
@@ -89,14 +84,14 @@ public class ProblemFragment extends Fragment {
         });
 
         return rootView;
-        //return inflater.inflate(R.layout.fragment_problem, container, false);
-    }
 
+
+    }
 
     public void ceateData(){
 
         //** 더미 데이터
-        String fName="근육몬";
+        String fName="괴력몬";
         switch (fName) {
             case "근육몬":
                 chats.add(new VM_Data_CHAT("근육몬", "어떤 문제를 도와줄까요?",0, 0));
@@ -107,11 +102,10 @@ public class ProblemFragment extends Fragment {
             case "괴력몬":
                 chats.add(new VM_Data_CHAT("괴력몬", "안녕하세요",0 ,0));
                 chats.add(new VM_Data_CHAT("괴력몬", "이문제를 잘 모르겠어요~",1,R.drawable.img_contract));
-                chats.add(new VM_Data_CHAT("괴력몬", "감사합니다",0,0));
+                chats.add(new VM_Data_CHAT("괴력몬", "푸는 방법을 알려드릴게요",0,0));
+                chats.add(new VM_Data_CHAT("괴력몬", "감사합니다~",1,R.drawable.img_contract));
                 break;
         }
 
     }
-
-
 }
--- app/src/main/java/com/example/visualmath/VM_TeacherSolveProblemActivity.java
+++ app/src/main/java/com/example/visualmath/VM_TeacherSolveProblemActivity.java
@@ -1,15 +1,52 @@
 package com.example.visualmath;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class VM_TeacherSolveProblemActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
+    SolveFragment solveFragment;
+    TeacherProblemFragment teacherProblemFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vm__teacher_solve_problem);
 
+        // ActionBar 숨기기
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
+
+        init();
+    }
+
+    public void init(){
+        fragmentManager=getSupportFragmentManager();
+        solveFragment=new SolveFragment();
+        teacherProblemFragment=new TeacherProblemFragment();
+        transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.teacher_solve_container,solveFragment).commitAllowingStateLoss();
+
+    }
+
+    public void showUsersLoad(View view) {
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.teacher_solve_container, solveFragment).commitAllowingStateLoss();
+    }
+
+    public void showSolve(View view) {
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.teacher_solve_container, teacherProblemFragment).commitAllowingStateLoss();
+    }
+
+    public void showBoth(View view) {
     }
 }
