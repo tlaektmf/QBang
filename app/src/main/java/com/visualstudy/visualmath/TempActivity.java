00fbd1da0187e59d746e18863aa9cfef4b51ff3c
tlaektmf <dazzel0826@gmail.com>
2020-08-07 23:01:32 +0900

    Revert "Revert "find project""
    This reverts commit c9c05668cfc10dfd0fe161a8a9a46ee8fbe4b9b7.

---
+++ app/src/main/java/com/visualstudy/visualmath/TempActivity.java
@@ -0,0 +1,49 @@
+package com.visualstudy.visualmath;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.visualstudy.visualmath.R;
+
+public class TempActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+
+        //
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(VM_ENUM.TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        ///String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(VM_ENUM.TAG, token);
+                        ///Toast.makeText(TempActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(TempActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+    }
+
+
+}
