11af3dcc8654741d18f6d9eed82cde68e65a827c
tlaektmf <dazzel0826@gmail.com>
2020-08-07 22:59:14 +0900

    find project

---
+++ app/src/main/java/com/visualstudy/visualmath/MyFirebaseInstanceIDService.java
@@ -0,0 +1,8 @@
+package com.visualstudy.visualmath;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
+
+
+}
